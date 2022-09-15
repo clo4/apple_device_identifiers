feb5362c4de3fec3cbca337b801f06319262eb45
Robert <52195359+SeparateRecords@users.noreply.github.com>
2022-09-15 19:07:53 +1000

    Add IDs file and add to JS

--- build.ts
+++ build.ts
@@ -1,20 +1,26 @@
 #!/usr/bin/env deno --quiet --allow-all
 
-import * as path from "https://deno.land/std@0.130.0/path/mod.ts";
-import { debounce } from "https://deno.land/std@0.130.0/async/debounce.ts";
+import * as path from "https://deno.land/std@0.155.0/path/mod.ts";
+import { debounce } from "https://deno.land/std@0.155.0/async/debounce.ts";
 
 function timer() {
   const start = performance.now();
   return () => performance.now() - start;
 }
 
-async function generate(init: {
+type GenerateInit = {
   inputDirectory: string;
-  outputJson: string;
+  outputJsonDevices: string;
+  outputJsonIds: string;
   outputTypescript: string;
-}) {
-  const { inputDirectory, outputJson, outputTypescript } = init;
+};
 
+async function generate({
+  inputDirectory,
+  outputJsonDevices,
+  outputJsonIds,
+  outputTypescript,
+}: GenerateInit) {
   const time = timer();
 
   const readPromises = [...Deno.readDirSync(inputDirectory)]
@@ -27,7 +33,13 @@ async function generate(init: {
   const files = await Promise.all(readPromises);
   const objects = files.map((text) => JSON.parse(text));
   const data = Object.assign({}, ...objects) as Record<string, string>;
-  const devices = JSON.stringify(data, null, 2);
+  const idsToNames = {} as Partial<Record<string, string[]>>;
+  for (const [name, id] of Object.entries(data)) {
+    const arr = (idsToNames[id] ??= [] as string[]);
+    arr.push(name);
+  }
+  const ids = JSON.stringify(idsToNames);
+  const devices = JSON.stringify(data);
 
   // 'YYYY-MM-DD'.length === 10
   const yyyymmdd = new Date().toISOString().slice(0, 10);
@@ -45,33 +57,57 @@ async function generate(init: {
 export const devices = ${devices} as const;
 
 /**
+ * A map of device identifier to device name.
+ */
+export const ids = ${ids} as const;
+
+/**
  * A type that maps device name to device identifier.
  */
 export type Devices = typeof devices;
+  
+/**
+ * An exclusive union of device names. Only currently known device names
+ * can be assigned to this type.
+ *
+ * Most of the time, \`AnyDeviceName\` is a better fit as it also allows
+ * any string to be assigned to it.
+ */
+export type DeviceName = keyof typeof devices;
 
 /**
  * An exclusive union of device identifiers. Only currently known identifiers
  * can be assigned to this type.
  *
  * Most of the time, \`AnyIdentifier\` is a better fit as it also allows
- * any string to be assigned to it
+ * any string to be assigned to it.
  */
 export type Identifier = Devices[keyof Devices];
 
 // All strings can be assigned to \`string & {}\`, but because it's a distinct
 // type from \`string\`, the compiler can't simplify the type.
+  
+// deno-lint-ignore ban-types
+type StringSuggestions<T extends string> = T | (string & {});
 
 /**
- * An union of device identifiers. Any string can be assigned to this
+ * A union of device identifiers. Any string can be assigned to this
  * type, which allows for future identifiers that were unknown at the time this
  * list was created.
  */
-// deno-lint-ignore ban-types
-export type AnyIdentifier = Identifier | (string & {});
+export type AnyIdentifier = StringSuggestions<Identifier>;
+
+/**
+ * A union of device names. Any string can be assigned to this
+ * type, which allows for future devices that were unknown at the time this
+ * list was created.
+ */
+export type AnyDeviceName = StringSuggestions<DeviceName>;
 `.trimStart();
 
   await Promise.all([
-    Deno.writeTextFile(outputJson, devices + "\n"),
+    Deno.writeTextFile(outputJsonDevices, devices + "\n"),
+    Deno.writeTextFile(outputJsonIds, ids + "\n"),
     Deno.writeTextFile(outputTypescript, mod),
   ]);
 
@@ -84,10 +120,17 @@ async function main() {
   const thisDir = path.dirname(thisFile);
 
   const inputDirectory = path.join(thisDir, "devices");
-  const outputJson = path.join(thisDir, "devices.json");
+  const outputJsonDevices = path.join(thisDir, "devices.json");
+  const outputJsonIds = path.join(thisDir, "ids.json");
   const outputTypescript = path.join(thisDir, "mod.ts");
 
-  const run = () => generate({ inputDirectory, outputJson, outputTypescript });
+  const run = () =>
+    generate({
+      inputDirectory,
+      outputJsonDevices,
+      outputJsonIds,
+      outputTypescript,
+    });
 
   if (Deno.args.includes("--watch")) {
     console.log("Building and watching for changes, ^c to exit");
