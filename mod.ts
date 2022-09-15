80a90fe447a9b640aaa487632d5178d410072165
Robert <52195359+SeparateRecords@users.noreply.github.com>
2022-09-15 19:17:05 +1000

    Update code

--- mod.ts
+++ mod.ts
@@ -717,7 +717,7 @@ export type DeviceName = keyof typeof devices;
  * Most of the time, `AnyIdentifier` is a better fit as it also allows
  * any string to be assigned to it.
  */
-export type Identifier = keyof identifiers;
+export type Identifier = keyof typeof identifiers;
 
 // All strings can be assigned to `string & {}`, but because it's a distinct
 // type from `string`, the compiler can't simplify the type.
